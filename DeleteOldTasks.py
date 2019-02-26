c0e50067b4d0ecc5db4eb337ee7e2a7a1eeeb28a
Chris Rice <CRE@qlik.com>
2019-02-26 13:59:53 +0100

    comments

--- DeleteOldTasks.py
+++ DeleteOldTasks.py
@@ -5,10 +5,11 @@
 TaskList = []
 
 #Loop through list
-for x in TaskList:
+#for x in TaskList:
     #get task info via api
     #https://help.qlik.com/en-US/qlikview-developer/12.1/APIs/QMS+API/html/Properties_T_PIX_QMSAPI_DataObjects_TaskStatus_TaskStatusExtended.htm
 
-    if Today - LastUpdated > 60:
+    #if Today - LastUpdated > 60:
         #delete task via API
         #https://help.qlik.com/en-US/qlikview-developer/12.1/APIs/QMS+API/html/M_PIX_Services_V12_Api2_IQMS2_DeleteTask.htm
+        
