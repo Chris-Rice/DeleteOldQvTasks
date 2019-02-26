1caa3ab89573a0b7cef372975080be69c5ebbf2c
Chris Rice <CRE@qlik.com>
2019-02-26 13:43:35 +0100

    initial  Please enter the commit message for your changes. Lines starting

---
+++ DeleteOldTasks.py
@@ -0,0 +1,14 @@
+#Get task list via API
+#https://help.qlik.com/en-US/qlikview-developer/12.1/APIs/QMS+API/html/M_PIX_Services_V12_Api2_IQMS2_FindTask.htm
+
+#Write task list to list
+TaskList = []
+
+#Loop through list
+for x in TaskList:
+    #get task info via api
+    #https://help.qlik.com/en-US/qlikview-developer/12.1/APIs/QMS+API/html/Properties_T_PIX_QMSAPI_DataObjects_TaskStatus_TaskStatusExtended.htm
+
+    if Today - LastUpdated > 60:
+        #delete task via API
+        #https://help.qlik.com/en-US/qlikview-developer/12.1/APIs/QMS+API/html/M_PIX_Services_V12_Api2_IQMS2_DeleteTask.htm
